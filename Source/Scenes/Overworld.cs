0085c70539f981306df37664163f55a6ae41a297
Axi <98420477+axiand@users.noreply.github.com>
2024-02-12 03:21:12 +0100

    Add vanilla options to main menu

--- Source/Scenes/Overworld.cs
+++ Source/Scenes/Overworld.cs
@@ -256,6 +256,16 @@ public class Overworld : Scene
 				Paused = !Paused;
 
 				if (Paused) {
+					Menu optionsMenu = new Menu();
+					optionsMenu.Title = Loc.Str("OptionsTitle");
+					optionsMenu.Add(new Menu.Toggle(Loc.Str("OptionsFullscreen"), Save.Instance.ToggleFullscreen, () => Save.Instance.Fullscreen));
+					optionsMenu.Add(new Menu.Toggle(Loc.Str("OptionsZGuide"), Save.Instance.ToggleZGuide, () => Save.Instance.ZGuide));
+					optionsMenu.Add(new Menu.Toggle(Loc.Str("OptionsTimer"), Save.Instance.ToggleTimer, () => Save.Instance.SpeedrunTimer));
+					optionsMenu.Add(new Menu.MultiSelect<Save.InvertCameraOptions>(Loc.Str("OptionsInvertCamera"), Save.Instance.SetCameraInverted, () => Save.Instance.InvertCamera));
+					optionsMenu.Add(new Menu.Spacer());
+					optionsMenu.Add(new Menu.Slider(Loc.Str("OptionsBGM"), 0, 10, () => Save.Instance.MusicVolume, Save.Instance.SetMusicVolume));
+					optionsMenu.Add(new Menu.Slider(Loc.Str("OptionsSFX"), 0, 10, () => Save.Instance.SfxVolume, Save.Instance.SetSfxVolume));
+
 					pauseMenu = new();
 					pauseMenu.Title = Loc.Str("PauseOptions");
 
@@ -266,6 +276,7 @@ public class Overworld : Scene
 					};
 
 					pauseMenu.Add(new Menu.Submenu(Loc.Str("PauseMods"), pauseMenu, modMenu));
+					pauseMenu.Add(new Menu.Submenu(Loc.Str("PauseOptions"), pauseMenu, optionsMenu));
 					pauseMenu.Add(new Menu.Option(Loc.Str("Exit"), () =>
 					{
 						if (Game.Instance.NeedsReload) {
