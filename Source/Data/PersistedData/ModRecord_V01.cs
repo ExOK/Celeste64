d2293dfa9ab1208433e4935fe88747dbae5e5a1e
Jasmine Stephens <jasminegamedev@gmail.com>
2024-03-24 10:29:24 -0700

    deleting unneeded functions.

--- Source/Data/PersistedData/ModRecord_V01.cs
+++ Source/Data/PersistedData/ModRecord_V01.cs
@@ -26,54 +26,6 @@ public class ModRecord_V01 : PersistedData
 	public Dictionary<string, int> SettingsIntData { get; set; } = [];
 	public Dictionary<string, float> SettingsFloatData { get; set; } = [];
 	public Dictionary<string, bool> SettingsBoolData { get; set; } = [];
-
-	public string GetString(string name, string defaultValue = "")
-		=> StringData.TryGetValue(name, out string? value) ? value : defaultValue;
-
-	public string SetString(string name, string value = "")
-		=> StringData[name] = value;
-
-	public int GetInt(string name, int defaultValue = 0)
-		=> IntData.TryGetValue(name, out int value) ? value : defaultValue;
-
-	public int SetInt(string name, int value = 1)
-		=> IntData[name] = value;
-
-	public float GetFloat(string name, float defaultValue = 0)
-		=> FloatData.TryGetValue(name, out float value) ? value : defaultValue;
-
-	public float SetFloat(string name, float value = 1)
-		=> FloatData[name] = value;
-
-	public bool GetBool(string name, bool defaultValue = false)
-		=> BoolData.TryGetValue(name, out bool value) ? value : defaultValue;
-
-	public bool SetBool(string name, bool value = false)
-		=> BoolData[name] = value;
-
-	public string SettingsGetString(string name, string defaultValue = "")
-		=> SettingsStringData.TryGetValue(name, out string? value) ? value : defaultValue;
-
-	public string SettingsSetString(string name, string value = "")
-		=> SettingsStringData[name] = value;
-
-	public int SettingsGetInt(string name, int defaultValue = 0)
-		=> SettingsIntData.TryGetValue(name, out int value) ? value : defaultValue;
-
-	public int SettingsSetInt(string name, int value = 1)
-		=> SettingsIntData[name] = value;
-
-	public float SettingsGetFloat(string name, float defaultValue = 0)
-		=> SettingsFloatData.TryGetValue(name, out float value) ? value : defaultValue;
-
-	public float SettingsSetFloat(string name, float value = 1)
-		=> SettingsFloatData[name] = value;
-
-	public bool SettingsGetBool(string name, bool defaultValue = false)
-		=> SettingsBoolData.TryGetValue(name, out bool value) ? value : defaultValue;
-
-	public bool SettingsSetBool(string name, bool value = false)
-		=> SettingsBoolData[name] = value;
 }
 
 internal class ModRecord_V01Converter : JsonConverter<ModRecord_V01>
