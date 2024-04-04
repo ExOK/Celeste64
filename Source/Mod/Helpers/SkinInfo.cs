98c4f8f095a3cf80e5d395281bde987b9559bb4e
Jasmine Stephens <jasminegamedev@gmail.com>
2024-04-03 21:39:25 -0700

    Updating skins to allow hex strings for hair colors.
    Also adding better defaults. It now defaults to Madeline's hair colors, instead of black.

--- Source/Mod/Helpers/SkinInfo.cs
+++ Source/Mod/Helpers/SkinInfo.cs
@@ -1,4 +1,5 @@
 
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Celeste64;
@@ -12,11 +13,18 @@ public class SkinInfo
 	public virtual string Model { get; set; } = "";
 	public virtual bool HideHair { get; set; } = false;
 	public virtual string CollectableId { get; set; } = "";
-	public virtual int HairNormal { get; set; } = 0;
-	public virtual int HairNoDash { get; set; } = 0;
-	public virtual int HairTwoDash { get; set; } = 0;
-	public virtual int HairRefillFlash { get; set; } = 0;
-	public virtual int HairFeather { get; set; } = 0;
+
+	[JsonConverter(typeof(DecimalOrHexConverter))]
+	public virtual int HairNormal { get; set; } = 0xdb2c00;
+	[JsonConverter(typeof(DecimalOrHexConverter))]
+	public virtual int HairNoDash { get; set; } = 0x6ec0ff;
+	[JsonConverter(typeof(DecimalOrHexConverter))]
+	public virtual int HairTwoDash { get; set; } = 0xfa91ff;
+	[JsonConverter(typeof(DecimalOrHexConverter))]
+	public virtual int HairRefillFlash { get; set; } = 0xffffff;
+	[JsonConverter(typeof(DecimalOrHexConverter))]
+	public virtual int HairFeather { get; set; } = 0xf2d450;
+
 
 	public bool IsValid()
 	{
@@ -30,6 +38,39 @@ public class SkinInfo
 
 	public virtual void OnEquipped(Player player, Model m) { }
 	public virtual void OnRemoved(Player player, Model m) { }
+}
+
+// This allows us to support either decimal integers or hex strings.
+public class DecimalOrHexConverter : JsonConverter<int>
+{
+	public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		if (reader.TokenType == JsonTokenType.Number)
+		{
+			return reader.TryGetInt32(out var i) ? i : 0;
+		}
+		else if (reader.TokenType == JsonTokenType.String)
+		{
+			try
+			{
+				return Convert.ToInt32(reader.GetString(), 16);
+			}
+			catch (Exception ex)
+			{
+				Log.Error("Error: Could not parse value in skin file: " + ex.ToString());
+				return 0;
+			}
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
+	{
+		JsonSerializer.Serialize(writer, value, value.GetType());
+	}
 }
 
 [JsonSourceGenerationOptions(WriteIndented = true)]
